9fdd0633e6e14215c04ecd10f45eea9cd88ca154
osmar-avell <osmarfilho6@gmail.com>
2022-06-26 11:36:42 -0300

    Eliminação dos itens do menu

---
+++ src/app/app.menu.component.ts
@@ -0,0 +1,95 @@
+import { Component, OnInit } from '@angular/core';
+import { AppMainComponent } from './app.main.component';
+
+@Component({
+    selector: 'app-menu',
+    template: `
+        <div class="layout-menu-container">
+            <ul class="layout-menu" role="menu" (keydown)="onKeydown($event)">
+                <li app-menu class="layout-menuitem-category" *ngFor="let item of model; let i = index;" [item]="item" [index]="i" [root]="true" role="none">
+                    <div class="layout-menuitem-root-text" [attr.aria-label]="item.label">{{item.label}}</div>
+                    <ul role="menu">
+                        <li app-menuitem *ngFor="let child of item.items" [item]="child" [index]="i" role="none"></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    `
+})
+export class AppMenuComponent implements OnInit {
+
+    model: any[];
+
+    constructor(public appMain: AppMainComponent) { }
+
+    ngOnInit() {
+        this.model = [
+            {
+                label: 'Home',
+                items:[
+                    {label: 'Dashboard',icon: 'pi pi-fw pi-home', routerLink: ['/']}
+                ]
+            },
+            {
+                label: 'Pages',
+                items: [
+                    {label: 'Login', icon: 'pi pi-fw pi-sign-in', routerLink: ['pages/login']},
+                    {label: 'Error', icon: 'pi pi-fw pi-times-circle', routerLink: ['pages/error']},
+                    {label: 'Not Found', icon: 'pi pi-fw pi-exclamation-circle', routerLink: ['pages/notfound']},
+                    {label: 'Access Denied', icon: 'pi pi-fw pi-lock', routerLink: ['pages/access']},
+                    {label: 'Empty', icon: 'pi pi-fw pi-circle', routerLink: ['/pages/empty']}
+                ]
+            },
+            {
+                label: 'Hierarchy',
+                items: [
+                    {
+                        label: 'Submenu 1', icon: 'pi pi-fw pi-bookmark',
+                        items: [
+                            {
+                                label: 'Submenu 1.1', icon: 'pi pi-fw pi-bookmark',
+                                items: [
+                                    {label: 'Submenu 1.1.1', icon: 'pi pi-fw pi-bookmark'},
+                                    {label: 'Submenu 1.1.2', icon: 'pi pi-fw pi-bookmark'},
+                                    {label: 'Submenu 1.1.3', icon: 'pi pi-fw pi-bookmark'},
+                                ]
+                            },
+                            {
+                                label: 'Submenu 1.2', icon: 'pi pi-fw pi-bookmark',
+                                items: [
+                                    {label: 'Submenu 1.2.1', icon: 'pi pi-fw pi-bookmark'}
+                                ]
+                            },
+                        ]
+                    },
+                    {
+                        label: 'Submenu 2', icon: 'pi pi-fw pi-bookmark',
+                        items: [
+                            {
+                                label: 'Submenu 2.1', icon: 'pi pi-fw pi-bookmark',
+                                items: [
+                                    {label: 'Submenu 2.1.1', icon: 'pi pi-fw pi-bookmark'},
+                                    {label: 'Submenu 2.1.2', icon: 'pi pi-fw pi-bookmark'},
+                                ]
+                            },
+                            {
+                                label: 'Submenu 2.2', icon: 'pi pi-fw pi-bookmark',
+                                items: [
+                                    {label: 'Submenu 2.2.1', icon: 'pi pi-fw pi-bookmark'},
+                                ]
+                            },
+                        ]
+                    }
+                ]
+            },
+        ];
+    }
+
+    onKeydown(event: KeyboardEvent) {
+        const nodeElement = (<HTMLDivElement> event.target);
+        if (event.code === 'Enter' || event.code === 'Space') {
+            nodeElement.click();
+            event.preventDefault();
+        }
+    }
+}
